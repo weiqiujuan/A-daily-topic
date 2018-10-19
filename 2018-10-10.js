7d5150c38a0728d09b8f21323e1a91f34c756223
CallmePapa <1062927638@qq.com>
2018-10-19 11:18:46 +0800

    统计字符次数

--- 2018-10-10.js
+++ 2018-10-10.js
@@ -1,1 +1,22 @@
 //统计字符出现次数
+
+function countStr(str) {
+    let obj = {}
+    for (let i = 0; i < str.length; i++) {
+        let v = str.charAt(i)
+        if (obj[v] && obj[v].value === v) {
+            obj[v].count = ++obj[v].count
+        } else {
+            obj[v] = {};
+            obj[v].count = 1;
+            obj[v].value = v;
+        }
+    }
+    for (let key in obj) {
+        console.log(obj[key].value + '=' + obj[key].count)
+    }
+    return obj
+}
+
+let test = 'weiqiujuan,weiqiujuan'
+console.log(countStr(test));
